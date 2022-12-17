7dfb76687c4cbc09b02b539eac9a9369955b5c78
haisenheim <clementessomba@gmail.com>
2022-12-17 13:20:17 +0100

    first in essai branch

---
+++ resources/views/Filieres/create.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/adminlte.css') }}">
+    <title>Filieres</title>
+</head>
+<body>
+
+    <div style="height: 300px; padding: 2rem;" class="container-fluid" id="header">
+        <h1>GESTION DES FILIERES</h1>
+    </div>
+
+</body>
+</html>
